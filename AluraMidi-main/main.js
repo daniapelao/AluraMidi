74e0a67cbc30c2a6fa5fa4e763712b73827f6691
daniapelao <126569139+daniapelao@users.noreply.github.com>
2023-10-17 08:00:51 -0300

    Update main.js

--- AluraMidi-main/main.js
+++ AluraMidi-main/main.js
@@ -18,3 +18,31 @@ document.querySelector('#som_tecla_tom').play();
 }
 document.querySelector('.tecla_tom').onclick = tocaSomTom;
 
+function tocaSomToim(){
+document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomSplash(){
+document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomPuff(){
+document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomTim(){
+document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomClap(){
+document.querySelector('#som_tecla_Clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
+
+
